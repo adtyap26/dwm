e6d9ebc82083f24e02ed6e169b6641b1ff80004a
Aditya Permana <permanaaditya2606@gmail.com>
2022-12-01 10:20:09 +0700

    update dwm

--- config.def.h
+++ config.def.h
@@ -40,7 +40,7 @@ static const int vertpadbar         = 7;        /* vertical padding for statusba
 
 /* tagging */
 // static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "web", "dev", "sys", "doc", "mult", "dir", "set" };
+static const char *tags[] = { "web", "dev", "chat", "doc", "mult", "dir", "set" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -49,10 +49,11 @@ static const Rule rules[] = {
 	 *  use tags mask to point an application to a specific workspace
 	 */
 	/* class                    instance              title                        tags mask      iscentered     isfloating       monitor */
+
     { "Google-chrome",        NULL,                 NULL,                         1,             0,            0,           -1 },
-    { "TelegramDesktop",      NULL,                 NULL,                         1 << 4,        0,            0,           -1 },
-    { "discord",              NULL,                 NULL,                         1 << 4,        0,            0,           -1 },
-    { "Thunderbird",          NULL,                 NULL,                         1 << 6,        0,            0,           -1 },
+    { "TelegramDesktop",      NULL,                 NULL,                         1 << 2,        0,            0,           -1 },
+    { "discord",              NULL,                 NULL,                         1 << 2,        0,            0,           -1 },
+    { "Thunderbird",          NULL,                 NULL,                         1 << 2,        0,            0,           -1 },
     { "mpv",                  NULL,                 NULL,                         1 << 4,        1,            1,           -1 },
     { NULL,                   "libreoffice",        NULL,                         1 << 3,        0,            0,           -1 },
     { "wps",                  NULL,                "WPS Writer",                  1 << 3,        0,            0,           -1 },
@@ -64,10 +65,11 @@ static const Rule rules[] = {
     { "St",                   NULL,                 "Ranger",                     0,             0,            0,           -1 },
     { "Postman",              NULL,                 NULL,                         0,             0,            0,           -1 },
     { "vlc",                  NULL,                 NULL,                         1 << 4,        0,            0,           -1 },
+    { "st-256color",          NULL,                 "nmtui",                      1 << 6,        1,            1,           -1 },
     { "Evince",               NULL,                 "Recent Documents",           1 << 3,        0,            0,           -1 },
     /* class                  instance              title                         tags mask      iscentered    isfloating   monitor */
     /* Floating */
-    { "SimpleScreenRecorder", NULL,                 NULL,                         0,             1,            1,           -1 },
+    { "SimpleScreenRecorder", NULL,                 NULL,                         1 << 6,        1,            1,           -1 },
     { "Thunar",               "thunar",             "File Operation Progress",    0,             0,            1,           -1 },
     { "GParted",              NULL,                 NULL,                         0,             1,            1,           -1 },
   };
